62325c7607badc33d862d4fd7c9f1d1397988294
Abduvohid <vohid4970@gmail.com>
2022-11-15 15:43:31 +0500

    tracking add phone and driver_num

--- src/routes/tracking.js
+++ src/routes/tracking.js
@@ -6,6 +6,8 @@ module.exports = () => {
     driver: faker.name.fullName({ sex: "male" }),
     truck_number: faker.datatype.number({ min: 1000, max: 9999 }),
     status: faker.helpers.arrayElement(["DR", "SB", "ON", "OFF"]),
+    phone: faker.phone.number("###-###-####"),
+    driver_num: faker.datatype.number({ min: 1000 }),
     coordinates: new Array(2).fill(undefined).map(() => ({
       lat: Number(faker.address.latitude()),
       lng: Number(faker.address.longitude()),
