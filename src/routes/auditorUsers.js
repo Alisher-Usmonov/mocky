d86be5a2af268a23a3bf329c5ef59913e6eec6c6
Xushnud9812 <hushnud1208@mail.ru>
2022-11-29 10:30:36 +0500

    edit auditor Users

--- src/routes/auditorUsers.js
+++ src/routes/auditorUsers.js
@@ -3,9 +3,9 @@ const { faker } = require("@faker-js/faker");
 module.exports = () => {
   const users = new Array(100).fill(undefined).map((_, index) => ({
     id: index + 1,
-    first_name: faker.name.firstName(),
-    last_name: faker.name.lastName(),
-    user_name: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    userName: faker.internet.userName(),
     email: faker.internet.email(),
     phone: faker.phone.number("### ## ### ## ##"),
   }));
